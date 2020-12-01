0986a677a5c8e8366a789f88d772c6b007a10beb
CaKellum <cakellum@bsu.edu>
2020-11-30 19:27:33 -0500

    Adds Functionallity to UI

--- src/MainUI.java
+++ src/MainUI.java
@@ -1,7 +1,11 @@
 import javax.swing.*;
-import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.EventListener;
 
 public class MainUI {
+
+    private int clicks =0;
 
     public MainUI(){
         JFrame appFrame = new JFrame("Button Counter");
@@ -14,7 +18,17 @@ public class MainUI {
     private JPanel getPane() {
         JPanel appPanel = new JPanel();
         JButton appButton = new JButton("Click Me!");
+        JTextArea appText  = new JTextArea(clicks+"");
+        appButton.addActionListener( new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clicks +=1;
+                appText.setText(clicks+"");
+            }
+        });
 
+        appPanel.add(appText);
+        appPanel.add(appButton);
         return appPanel;
     }
 
